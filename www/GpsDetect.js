8a896c1ae9acca723ba94b7a84d5e5b750a82e95
Julen Irazoki <julen@iametza.com>
2014-10-27 11:22:41 +0100

    zuzenketak

--- www/GpsDetect.js
+++ www/GpsDetect.js
@@ -1,10 +1,9 @@
 var cordova = require('cordova'),
     exec = require('cordova/exec');
 
-var gpsDetect = {
-    checkGPS: function(successCallback, failureCallback) {
+function GpsDetect(){};
+GpsDetect.prototype.checkGPS = function(successCallback, failureCallback) {
         exec(successCallback, failureCallback, 'GpsDetection', 'gpsDetection', []);
     }
-};
-var gpsDetect = new gpsDetect();
+var gpsDetect = new GpsDetect();
 module.exports = gpsDetect;
